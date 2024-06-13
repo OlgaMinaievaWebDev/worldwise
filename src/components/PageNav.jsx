1509460c14f37a3212daa729b272751a603cde36
Olga <minaeva9@gmail.com>
2024-06-13 14:27:48 -0400

    update components

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -8,16 +8,15 @@ function PageNav() {
       <Logo />
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
         </li>
         <li>
-          <NavLink to="'/login">Login</NavLink>
+          <NavLink to="'/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
