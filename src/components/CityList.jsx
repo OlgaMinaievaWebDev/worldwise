e6608e8154635fa4dd62d5b64c455a1c5f2f8dc0
Olga <minaeva9@gmail.com>
2024-06-15 13:23:20 -0400

    cityList updated

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,10 +1,14 @@
 import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
+import Message from "./Message";
 
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
-  if (!cities.length) return <Message messages='Add your first city/>;
+
+  if (!cities.length)
+    return <Message message="Add your first city by clicking on a city map" />;
+
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
