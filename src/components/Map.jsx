3c43359f3621e5983cc972c88b26e2668ab60422
Olga <minaeva9@gmail.com>
2024-06-21 12:31:03 -0400

    including Map with Laeflet Library

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,18 +1,25 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { useState } from "react";
 
 import styles from "./Map.module.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 function Map() {
   const navigate = useNavigate();
+
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={false}>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
